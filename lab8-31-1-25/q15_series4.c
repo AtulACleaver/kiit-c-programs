6acbdafad387f40af6ae8acd5a1e9a6841662276
Atul Anand <atulcleaver@gmail.com>
2025-02-02 23:23:47 +0530

    q15 wrong

--- lab8-31-1-25/q15_series4.c
+++ lab8-31-1-25/q15_series4.c
@@ -1,20 +1,11 @@
-// WAP to print the series as 3 5 7 11 13 17..........n, where n is given by user.
+// WAP to print the series as 3 5 7 11 13 17..........n, where n is given by user using loop
 
 #include <stdio.h>
-int main(){
-    int i = 1, n, a = 2, b = 3, c;
-    printf("Value of N: ");
-    scanf("%d", &n);
-    printf("3 ");
-    for(i = 1; i <= n; i++){
-        c = a + b;
-        printf("%d ", c);
-        a = b;
-        b = c;
-    }
 
-    return 0;
+int main(){
+    
 }
+
 
 // Output:
 // Value of N: 10
